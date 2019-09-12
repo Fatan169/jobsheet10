e94f8413da3a1d962a37cd13965e2a488397040e
Fatan Virgian Nova <53498573+Fatan169@users.noreply.github.com>
2019-09-12 19:33:04 +0700

    Add files via upload

---
+++ coba2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet10;
+
+/**
+ *
+ * @author asus
+ */
+public class coba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    String[] nama = new String[8];
+    
+    // membuat array
+    String teman []= {"Tio", "James", "Mika", "Bashir", "Fatan", "Fadil", "Kiki", "Satrio"};
+    
+    // mengambil data array
+     System.out.println(" nama teman saya: " + teman[0] +" "+teman[1] +" "+ teman[2]+" "+teman[3]+" "+teman[4]+" "+teman[5]+" "+teman[6]+" "+teman[7]+" ");
+    }
+    
+}
