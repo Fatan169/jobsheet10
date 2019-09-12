537714797ba2b3269e0935c0b7aa7ae2bf60431e
Fatan Virgian Nova <53498573+Fatan169@users.noreply.github.com>
2019-09-12 19:26:01 +0700

    Add files via upload

---
+++ challange1.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet10;
+
+/**
+ *
+ * @author asus
+ */
+public class challange1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          String namateman [] = new String [10];
+                    namateman[0]= ("Satmika Antargata Ozora");
+                    namateman[1]= ("James Hestu Satria");
+                    namateman[2]= ("Kevin Arullah Herdiansyah");
+                    namateman[3]= ("Fadhillah Bashir Al Hakim");
+                    namateman[4]= ("Gregorius Devon Bramantyo");
+                    namateman[5]= ("Fakhar Reza Alamsyah");
+                    namateman[6]= ("Lukman Alfaris Hidayatullah");
+                    namateman[7]= ("Yeremia Rizky Paramasatya");
+                    namateman[8]= ("Yulius Jati Satriyo Utomo");
+                    namateman[9]= ("Muhammad Satrio Ajinoto");
+            
+            String Alamat[] = new String[10];
+                    Alamat[0]=("Banyuwangi");
+                    Alamat[1]=("Sulfat");
+                    Alamat[2]=("Sawojajar1");
+                    Alamat[3]=("Sawojajar2");
+                    Alamat[4]=("Frateran");
+                    Alamat[5]=("Malang");
+                    Alamat[6]=("Kepanjen");
+                    Alamat[7]=("Blimbing");
+                    Alamat[8]=("pakis");
+                    Alamat[9]=("Sulfat");
+                    
+            String NomorHP[] = new String[10];
+                   NomorHP[0] = ("082232175005");
+                   NomorHP[1] = ("082165319310");
+                   NomorHP[2] = ("085334783095");
+                   NomorHP[3] = ("089697048302");
+                   NomorHP[4] = ("081216287160");
+                   NomorHP[5] = ("082244681109");
+                   NomorHP[6] = ("082210092669");
+                   NomorHP[7] = ("081333836654");
+                   NomorHP[8] = ("083851929737");
+                   NomorHP[9] = ("081359418005");
+            
+            String Hobby[] = new String[10];
+                   Hobby[0] = ("Voli");
+                   Hobby[1] = ("Basket");
+                   Hobby[2] = ("Baca Buku");
+                   Hobby[3] = ("Morsing");
+                   Hobby[4] = ("NgeGame");
+                   Hobby[5] = ("Bertumbuk");
+                   Hobby[6] = ("MainBasket");
+                   Hobby[7] = ("Tidur");
+                   Hobby[8] = ("MainGame");
+                   Hobby[9] = ("Sepak Bola");
+                   
+            String Instagram[] = new String[10];
+                   Instagram[0] = ("antarozora");
+                   Instagram[1] = ("mr.sleppless_owl");
+                   Instagram[2] = ("tioarullah");
+                   Instagram[3] = ("Lethanaxeger");
+                   Instagram[4] = ("gregoriusdevon");
+                   Instagram[5] = ("rezaalam_04");
+                   Instagram[6] = ("I_ahha_");
+                   Instagram[7] = ("yeremia_rizky");
+                   Instagram[8] = ("yulius_jati");
+                   Instagram[9] = ("mhsatrioa");
+                   System.out.println(" 1. nama teman saya: " + namateman[3] +" 2. Alamat : "+Alamat[3]+" 3. Nomor HP : "+NomorHP[3]+" 4. Hobby : "+Hobby[3]+" 5. Akun Instagram : "+ Instagram[3] );
+                   System.out.println(" 1. nama teman saya: " + namateman[5] +" 2. Alamat : "+Alamat[5]+" 3. Nomor HP : "+NomorHP[5]+" 4. Hobby : "+Hobby[5]+" 5. Akun Instagram : "+ Instagram[5] );  
+    }
+    
+    
+}
